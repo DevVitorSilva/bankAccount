35c624680c1978e3086872ca38dfbf0c810255e9
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-17 15:58:18 -0300

    add input holder, account number and initial deposit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter account holder: ");
+        String holder = sc.nextLine();
+        System.out.print("Enter account number: ");
+        int accountNumber = sc.nextInt();
+        System.out.print("Is there an initial deposit (y/n)? ");
+        char initialDeposit = sc.next().charAt(0);
+        sc.close();
 
     }
+
 }

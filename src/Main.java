dd79fe8fc479dd6463e35880c74aafe201968f01
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-17 17:02:07 -0300

    create first deposit and first withdraw

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,24 @@ public class Main {
         int accountNumber = sc.nextInt();
         System.out.print("Is there an initial deposit (y/n)? ");
         char initialDeposit = sc.next().charAt(0);
-        sc.close();
         CreateBankAccount bankAccount = new CreateBankAccount(holder, accountNumber);
         if(initialDeposit == 'y'){
-            bankAccount.deposit(initialDeposit);
+            System.out.print("Enter initial deposit value: ");
+            double firstDeposit = sc.nextDouble();
+            bankAccount.deposit(firstDeposit);
             bankAccount.printDataOnAccount();
         }else{
             bankAccount.printDataOnAccount();
         }
+        System.out.print("Enter deposit value: ");
+        double deposit = sc.nextDouble();
+        bankAccount.deposit(deposit);
+        bankAccount.printDataOnAccount();
+        System.out.print("Enter a withdraw value: ");
+        double withdraw = sc.nextDouble();
+        bankAccount.withdraw(withdraw);
+        bankAccount.printDataOnAccount();
+        sc.close();
     }
 
 }
